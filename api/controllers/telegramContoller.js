476a1f8200ca2101235007d3e24f35a853d69d0b
Ozan Botan <ozan.botan@lightspeedhq.com>
2020-04-17 20:20:15 +0300

    added post api logic to handle incoming inline queries from the via the Telegram Bot and to list a bunch of results from Spotify web api

--- api/controllers/telegramContoller.js
+++ api/controllers/telegramContoller.js
@@ -1,10 +1,70 @@
 exports.onUpdateReceived = function (req, res) {
-    // handle incoming Telegram payload here.
-    console.log(req.body.update_id);
-    //res.json({ status: "ok" });
-    res.json(req.body.update_id);
+    const fetch = require("node-fetch");
+    const Spotify = require("node-spotify-api");
+    const BOT_TOKEN = process.env.BOT_TOKEN;
+    if (req.body.inline_query) {
+        let messageId = req.body.inline_query.id;
+        let query = req.body.inline_query.query;
+        let userName = req.body.inline_query.from.username;
+        console.log("query => " + query);
+        console.log("userName => " + userName);
+        var spotify = new Spotify({
+            id: process.env.SPOTIFY_APP_ID,
+            secret: process.env.SPOTIFY_APP_SECRET,
+        });
+        const results = [];
+        spotify
+            .request(
+                "https://api.spotify.com/v1/shows/3umkGQAmujYDEwA8EXCypL/episodes?market=TR"
+            )
+            .then(function (data) {
+                if (data.items) {
+                    const sortedEpisodes = data.items.sort(
+                        (a, b) => b.release_date - a.release_date
+                    );
+                    sortedEpisodes.forEach((episode) => {
+                        results.push({
+                            type: "article",
+                            id: episode.id,
+                            title: episode.name,
+                            input_message_content: {
+                                message_text: episode.external_urls.spotify,
+                                parse_mode: "Markdown",
+                            },
+                            description: episode.description,
+                            thumb_url: episode.images[2].url,
+                            thumb_width: episode.images[2].width,
+                            thumb_height: episode.images[2].height,
+                        });
+                    });
+                    const telegramPayload = {
+                        inline_query_id: messageId,
+                        results: results,
+                    };
+                    fetch(
+                        "https://api.telegram.org/bot" +
+                            BOT_TOKEN +
+                            "/answerInlineQuery",
+                        {
+                            method: "post",
+                            body: JSON.stringify(telegramPayload),
+                            headers: { "Content-Type": "application/json" },
+                        }
+                    )
+                        .then((res) => res.json())
+                        .then((json) => console.log(json));
+                }
+            })
+            .catch(function (err) {
+                console.error("Error occurred: " + err);
+            });
+    }
+    res.json({ status: "ok" });
 };
 
 exports.onGetRequest = function (req, res) {
-    res.json({ status: "ok", message: "hello there!" });
+    res.json({
+        status: "ok",
+        message: "hey there! This path is functional for POST request only",
+    });
 };
